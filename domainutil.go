a64b561fb6f1013e1a767bf29c5de4685fbf6fff
jubobs <jubobs-oss@protonmail.com>
2020-11-21 15:37:27 +0100

    Precede error message by package name

--- domainutil.go
+++ domainutil.go
@@ -8,24 +8,26 @@ import (
 	ps "golang.org/x/net/publicsuffix"
 )
 
+const pkgName = "domainutil"
+
 var label = regexp.MustCompile(`[^\.]+`)
 
 func EffectiveTLDPlusN(domain string, n int) (string, error) {
 	domain = removeAtMostOneTrailingPeriod(domain)
 	if strings.HasPrefix(domain, ".") || strings.HasSuffix(domain, ".") || strings.Contains(domain, "..") {
-		return "", fmt.Errorf("empty label in domain %q", domain)
+		return "", fmt.Errorf("%s: empty label in domain %q", pkgName, domain)
 	}
 	suffix, _ := ps.PublicSuffix(domain)
 	if n == 0 {
 		return suffix, nil
 	}
 	if len(domain) <= len(suffix) { // the domain is only composed of its public suffix
-		return "", fmt.Errorf("cannot derive eTLD+%d for domain %q", n, domain)
+		return "", fmt.Errorf("%s: cannot derive eTLD+%d for domain %q", pkgName, n, domain)
 	}
 	i := len(domain) - len(suffix) - 1
 	pairs := label.FindAllStringIndex(domain[:i], -1)
 	if n > len(pairs) {
-		return "", fmt.Errorf("cannot derive eTLD+%d for domain %q", n, domain)
+		return "", fmt.Errorf("%s: cannot derive eTLD+%d for domain %q", pkgName, n, domain)
 	}
 	j := pairs[len(pairs)-n][0]
 	return domain[j:], nil
